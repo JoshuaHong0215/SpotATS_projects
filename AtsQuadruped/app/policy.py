cc2596fd235664c941aea9bb4556fe0c6688a544
JoshuaHong0215 <JoshuaHong0215>
2026-03-13 16:51:39 +0900

    코드 분석 및 주석 추가

--- AtsQuadruped/app/policy.py
+++ AtsQuadruped/app/policy.py
@@ -9,6 +9,8 @@ class PolicyRunner:
 
     def infer(self, obs_np):
         with torch.no_grad():
+            # 일반적인 파이썬박스 즉 Numpy배열을 Pytorch는 읽지 못한다.
+            # 그래서 AI전용 포장지인 Tensor로 데이터를 감싼다.
             t = torch.tensor(obs_np, dtype=torch.float32, device=self.device).unsqueeze(0)
             out = self.model(t).squeeze(0).cpu().numpy()
         return out
